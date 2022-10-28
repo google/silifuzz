3930f3144149974e28d3475ef31b37f24dd2b829
Doug Kwan <dougkwan@google.com>
2022-10-27 17:04:53 -0700

    Add a fuzz test for SnapRelocator.
    PiperOrigin-RevId: 484387143

---
+++ snap/snap_relocator_fuzz_test.cc
@@ -0,0 +1,45 @@
+// Copyright 2022 The SiliFuzz Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cstring>
+
+#include "fuzztest/fuzztest.h"
+#include "./snap/snap_relocator.h"
+#include "./util/mmapped_memory_ptr.h"
+
+namespace silifuzz {
+namespace {
+using ::fuzztest::Arbitrary;
+
+void RelocateRandomBytes(const std::string& bytes) {
+  MmappedMemoryPtr<char> relocatable;
+  if (bytes.empty()) {
+    relocatable = MakeMmappedMemoryPtr<char>(nullptr, 0);
+  } else {
+    relocatable = AllocateMmappedBuffer<char>(bytes.size());
+    memcpy(relocatable.get(), bytes.data(), bytes.size());
+  }
+
+  // This should not crash.
+  SnapRelocator::Error error;
+  auto corpus = SnapRelocator::RelocateCorpus(std::move(relocatable), &error);
+}
+
+constexpr size_t kMaxRandomCorpusSize = 1 << 16;
+
+FUZZ_TEST(FuzzDecodedInsn, RelocateRandomBytes)
+    .WithDomains(Arbitrary<std::string>().WithMaxSize(kMaxRandomCorpusSize));
+
+}  // namespace
+}  // namespace silifuzz
