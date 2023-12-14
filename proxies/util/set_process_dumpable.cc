0f44444effac3f63a709478d7e2cd6a699eabb35
Doug Kwan <dougkwan@google.com>
2023-12-14 14:28:57 -0800

    #silifuzz Fix PMU event proxy build breakage.
    Fix build breakage due to a missing dependency. Also add back a missing header inclusion.
    
    PiperOrigin-RevId: 591052745

--- proxies/util/set_process_dumpable.cc
+++ proxies/util/set_process_dumpable.cc
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#include "./proxies/util/set_process_dumpable.h"
 
 #include <sys/prctl.h>
 #include <sys/resource.h>
