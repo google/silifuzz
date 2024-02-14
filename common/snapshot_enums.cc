e7857be6a30411505c66199200e17833ad81634d
Doug Kwan <dougkwan@google.com>
2024-02-14 14:09:29 -0800

    Fix typo in MakerStopReason enum name and clean up header inclusion.
    PiperOrigin-RevId: 607100905

--- common/snapshot_enums.cc
+++ common/snapshot_enums.cc
@@ -14,9 +14,9 @@
 
 #include "./common/snapshot_enums.h"
 
+#include "absl/base/attributes.h"
 #include "./util/checks.h"
 #include "./util/itoa.h"
-#include "./util/logging_util.h"
 
 namespace silifuzz {
 namespace snapshot_types {
@@ -98,7 +98,7 @@ ABSL_CONST_INIT const char* EnumNameMap<snapshot_types::MakerStopReason>[7] = {
     "Endpoint",
     "AllPageLimit",
     "TimeBudget",
-    "HargSigSegv",
+    "HardSigSegv",
     "GeneralProtectionSigSegv",
     "SigTrap",
     "Signal",
