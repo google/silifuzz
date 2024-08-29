09914d59e3567b3dc094d708a9ecc84b6adf022a
SiliFuzz Team <noreply@google.com>
2024-08-28 22:00:24 -0700

    Support more than 1024 CPUs.
    PiperOrigin-RevId: 668758098

--- util/cpu_id.h
+++ util/cpu_id.h
@@ -18,6 +18,8 @@
 #include <errno.h>
 #include <sched.h>
 
+#include <array>
+
 #include "./util/checks.h"
 #include "./util/itoa.h"
 
@@ -28,6 +30,11 @@ inline constexpr int kAnyCPUId = -1;
 
 // CPU ID information not available.
 inline constexpr int kUnknownCPUId = -1;
+
+// The maximum number of CPUs supported by silifuzz.
+static constexpr int kMaxCpus = 2048;
+// The size of the CPU set in bytes.
+static constexpr int kCpuSetBytes = CPU_ALLOC_SIZE(kMaxCpus);
 
 // Returns the current CPU Id or kUnknownCPUId if that cannot be determined.
 int GetCPUId();
@@ -43,17 +50,21 @@ int SetCPUAffinity(int cpu_id);
 // Invoke a callback for each CPU in this threads affinity mask.
 template <typename F>
 inline void ForEachAvailableCPU(F callback) {
-  cpu_set_t all_cpus;
-  CPU_ZERO(&all_cpus);
-  bool success =
-      sched_getaffinity(0 /* this thread */, sizeof(all_cpus), &all_cpus) == 0;
+  // In the sched.h, each CPU occupies one bit.
+  // Declare a bit array with a size that is an integer multiple of cpu_set_t:
+  std::array<cpu_set_t,
+             (kCpuSetBytes + sizeof(cpu_set_t) - 1) / sizeof(cpu_set_t)>
+      all_cpus;
+  CPU_ZERO_S(kCpuSetBytes, all_cpus.data());
+  bool success = sched_getaffinity(0 /* this thread */, kCpuSetBytes,
+                                   all_cpus.data()) == 0;
   if (!success) {
     LOG_FATAL("Cannot get current CPU affinity mask: ", ErrnoStr(errno));
   }
 
   // Find all usable CPU.
-  for (int cpu = 0; cpu < CPU_SETSIZE; ++cpu) {
-    if (CPU_ISSET(cpu, &all_cpus)) {
+  for (int cpu = 0; cpu < kMaxCpus; ++cpu) {
+    if (CPU_ISSET_S(cpu, kCpuSetBytes, all_cpus.data())) {
       callback(cpu);
     }
   }
